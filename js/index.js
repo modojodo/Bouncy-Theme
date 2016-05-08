984580ffce89dbf4d672044a4334e2b2ab94628b
Umer Hassan <umerhassan94@gmail.com>
2016-05-08 14:00:48 -0400

    Modified the line spin.js visual

--- js/index.js
+++ js/index.js
@@ -4,23 +4,27 @@
 
 $(function () {
     var opts = {
-        lines: 20, // The number of lines to draw
-        length: 30, // The length of each line
-        width: 11, // The line thickness
-        radius: 10, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#BF55EC', // #rgb or #rrggbb or array of colors
-        speed: 1, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: '50%', // Top position relative to parent
-        left: '50%' // Left position relative to parent
-    };
+  lines: 9 // The number of lines to draw
+, length: 30 // The length of each line
+, width: 4 // The line thickness
+, radius: 14 // The radius of the inner circle
+, scale: 1 // Scales overall size of the spinner
+, corners: 1 // Corner roundness (0..1)
+, color: '#BF55EC' // #rgb or #rrggbb or array of colors
+, opacity: 0.25 // Opacity of the lines
+, rotate: 0 // The rotation offset
+, direction: 1 // 1: clockwise, -1: counterclockwise
+, speed: 1.4 // Rounds per second
+, trail: 60 // Afterglow percentage
+, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+, zIndex: 2e9 // The z-index (defaults to 2000000000)
+, className: 'spinner' // The CSS class to assign to the spinner
+, top: '50%' // Top position relative to parent
+, left: '50%' // Left position relative to parent
+, shadow: false // Whether to render a shadow
+, hwaccel: false // Whether to use hardware acceleration
+, position: 'absolute' // Element positioning
+};
     var target = document.getElementById('preloader');
     var spinner = new Spinner(opts).spin(target);
     $(window).on('load', function () {
